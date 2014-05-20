f359eccd0fa0e67a60f6a511f14eb3c4d28cf8da
Mark Lintern <markllintern@gmail.com>
2014-05-20 14:32:41 -0400

    new fav icon

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_GET["url"])) {
+    $url = $_GET["url"];
+    $xml = simplexml_load_file($url);
+    header('Content-type: application/xml');
+    $xml = $xml->asXML();
+} else {
+    $xml = 'Please send XML with request!  http://www.this.com/here.php?url=http://www.example.com/rss';
+}
+?>
+<?php print $xml ?>
