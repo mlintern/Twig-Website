29177305dca2b3f2bbd6964ab23eeee8005b97ec
Mark Lintern <mark.lintern@oracle.com>
2018-01-29 19:51:17 -0500

    Updated to twig 1.35 with composer

---
+++ README.md
@@ -0,0 +1,16 @@
+PHP Website built with Twig Templating system
+
+***If you run into any cache issues you may need to create directory and set permissions
+
+mkdir compilation_cache
+chmod 777 compilation_cache/
+
+
+***If you have not already generated the twig files you will need to do so with composer
+
+composer up
+
+
+***If you do no have composer installed you will need to install it.
+
+brew install composer
--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php
 
-require_once 'lib/Twig/Autoloader.php';
+require_once 'vendor/autoload.php';
 Twig_Autoloader::register();
 
 try{
 	$loader = new Twig_Loader_Filesystem('templates');
 	$twig = new Twig_Environment($loader, array(
-			//'debug' => TRUE,
-			'cache' => 'compilation_cache',
-			'auto_reload' => TRUE
-		));
+		//'debug' => TRUE,
+		'cache' => 'compilation_cache',
+		'auto_reload' => TRUE
+	));
 
 	$template_file = "default.html";
 
 	if (!empty($_GET["p"])){
 		$template_file = $_GET["p"].".html";
 	}
-	
+
 	$template = $twig->loadTemplate($template_file);
 
 	$docdatajson = '[{"title":"Website with PHP Twig","Author":"Mark"}]';
 	$docdata = json_decode($docdatajson, true);
 
 	$template->display(array(
-			'pageTitle' => 'PHP Twig Bootstrap Website',
-			'docs' => $docdata
-		));
+		'pageTitle' => 'PHP Twig Bootstrap Website',
+		'docs' => $docdata
+	));
 }catch (Exception $e) {
 	die ('ERROR: ' . $e->getMessage());
 }
